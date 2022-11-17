3dd91f9c4880e05fd8e4552399dd0c124546fe8a
HiagoRoque <medeiroshiago70@gmail.com>
2022-11-17 10:38:39 -0300

    primeiro teste de unidade exemplo

--- projeto/usuarios/tests.py
+++ projeto/usuarios/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class UsuarioViewTests(TestCase):
+
+    def test_login_usuario(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
