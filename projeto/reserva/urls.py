9fc94560686b961663f321eb9ba5850884dcb9bc
marlemM <marlem.magno@gmail.com>
2022-12-18 01:39:48 -0300

    atualizando reserva

--- projeto/reserva/urls.py
+++ projeto/reserva/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import homeR
+from .views import homeR, detalhar
 
 urlpatterns = [
     path('', homeR),
+    path('detalhar/<int:id>', detalhar, name="detalhar"),
 ]
--- projeto/reserva/views.py
+++ projeto/reserva/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, redirect
 from .models import Reserva
+from kit.models import Kit
 from django.views.decorators.http import require_http_methods
 
 @require_http_methods(["GET"])
 def homeR(request):
     reservar = Reserva.objects.all()
-    return render(request, "indexreserva.html", {"reservar": reservar})
+    kits = Kit.objects.all()
+    return render(request, "indexreserva.html", {"reservar": reservar,"kits":kits})
+
+@require_http_methods("GET")
+def detalhar(request):
+    reservar = reservar.objects.all()
+    return render(request, "/detalhar")
