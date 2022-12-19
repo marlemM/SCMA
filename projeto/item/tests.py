afe59c7f3e60f8a01dca92f62c29ba44ca223fcb
HiagoRoque <medeiroshiago70@gmail.com>
2022-12-19 18:01:33 -0300

    teste item

--- projeto/item/tests.py
+++ projeto/item/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
+import os
+import django
+from .models import Item
+
+class Item_test(TestCase):
+
+    def test_create_Item(self):
+
+        item = Item(nome='lapis')
+        self.assertEqual('lapis', item.nome, 'Testanto item')
+
+
+
 
 # Create your tests here.
