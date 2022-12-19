89c4057e5c7294eb9829adfd5fd93b7d10d9ed3e
HiagoRoque <medeiroshiago70@gmail.com>
2022-12-19 17:29:09 -0300

    teste usuário

--- projeto/usuario/tests.py
+++ projeto/usuario/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
+import os
+import django
+from .models import Usuario
+
+class Usuario_test(TestCase):
+
+    def test_create_usuario(self):
+
+        usuario = Usuario(nome='fulano', email='fulano@fu.com', senha='12345678')
+        self.assertEqual('fulano', usuario.nome, 'Testanto usuário')
+
+
+
 
 # Create your tests here.
