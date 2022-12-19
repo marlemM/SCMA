46cc8bf16963cc562963418eefb77fe2b94d75c8
HiagoRoque <medeiroshiago70@gmail.com>
2022-12-19 18:01:21 -0300

    teste usuário 2

--- projeto/usuario/tests.py
+++ projeto/usuario/tests.py
@@ -9,7 +9,13 @@ class Usuario_test(TestCase):
     def test_create_usuario(self):
 
         usuario = Usuario(nome='fulano', email='fulano@fu.com', senha='12345678')
-        self.assertEqual('fulano', usuario.nome, 'Testanto usuário')
+        self.assertEqual('fulano', usuario.nome)
+        self.assertEqual('fulano@fu.com', usuario.email)
+        self.assertEqual('12345678', usuario.senha)
+
+    def test_create_usuario2(self):
+        usuario = Usuario(nome='ciclano', email='ciclano@fu.com', senha='87654321')
+        self.assertEqual('87654321', usuario.senha)
 
 
 
